08ee4b3c61df2448e1f2fcbe156ebdcd36109bd6
solpyro <solpyro@gmail.com>
2022-02-08 17:39:03 +0200

    Fix positioning for the footer element

--- index.php
+++ index.php
@@ -7,39 +7,44 @@
 		<script type="text/javascript" src="js/countdown.js" ></script>
 	</head>
 	<body class="loading">
+		<div id="page-container">
 
 <?php include 'inc/menu.php' ?>
-
-		<div class="banner">
-			<h1 class="banner-title inside">Katrin Kivistik<br /><span>&amp;</span><br />Stuart Kemp</h1>
-		</div>
-		<h1 class="banner-title bottom">Katrin Kivistik<br /><span>&amp;</span><br />Stuart Kemp</h1>
-
-		<div class="countdown" id="countdown-en" lang="en">0 days, 0 hours, 0 minutes, 0 seconds</div>
-		<div class="countdown" id="countdown-et" lang="et">0 päeva, 0 tundi, 0 minutit, 0 sekundit</div>
-	
-		<div class="pure-g">
-			<div class="pure-u-1 pure-u-md-1-2 about-me">
-				<img src="img/katrin.jpg" class="photo" />
-				<h2 class="name">Katrin</h2>
-				<p lang="en" class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<p lang="et" class="about">ET Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			</div>
-			<div class="pure-u-1 pure-u-md-1-2 about-me">
-				<img src="img/stuart.jpg" class="photo" />
-				<h2 clas="name">Stuart</h2>
-				<p lang="en" class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<p lang="et" class="about">ET Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			</div>
-		</div>
 		
-		<div class="about-me">
-			<h2 lang="en" class="name">About us</h2>
-			<h2 lang="et" class="name">Meist</h2>
-			<p lang="en" class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<p lang="et" class="about">ET Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-		</div>
+			<div id="content-container">
+				<div class="banner">
+					<h1 class="banner-title inside">Katrin Kivistik<br /><span>&amp;</span><br />Stuart Kemp</h1>
+				</div>
+				<h1 class="banner-title bottom">Katrin Kivistik<br /><span>&amp;</span><br />Stuart Kemp</h1>
+
+				<div class="countdown" id="countdown-en" lang="en">0 days, 0 hours, 0 minutes, 0 seconds</div>
+				<div class="countdown" id="countdown-et" lang="et">0 päeva, 0 tundi, 0 minutit, 0 sekundit</div>
+			
+				<div class="pure-g">
+					<div class="pure-u-1 pure-u-md-1-2 about-me">
+						<img src="img/katrin.jpg" class="photo" />
+						<h2 class="name">Katrin</h2>
+						<p lang="en" class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<p lang="et" class="about">ET Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</div>
+					<div class="pure-u-1 pure-u-md-1-2 about-me">
+						<img src="img/stuart.jpg" class="photo" />
+						<h2 clas="name">Stuart</h2>
+						<p lang="en" class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<p lang="et" class="about">ET Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</div>
+				</div>
+				
+				<div class="about-me">
+					<h2 lang="en" class="name">About us</h2>
+					<h2 lang="et" class="name">Meist</h2>
+					<p lang="en" class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<p lang="et" class="about">ET Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div>
+			</div>
 
 <?php include 'inc/footer.php' ?>
+
+		</div>
 	</body>
 </html>
--- venues.php
+++ venues.php
@@ -6,28 +6,33 @@
 		<link rel="stylesheet" href="css/venues.css" />
 	</head>
 	<body class="loading">
-
+		<div id="page-container">
+			
 <?php include 'inc/menu.php' ?>
 
-		<div class="pure-g">
-			<div class="pure-u-1 pure-u-md-1-2 about-me">
-				<img src="img/karli-kirik.jpg" class="photo" />
-				<h2 lang="en" class="name">Kaarli Kirik</h2>
-				<h2 lang="et" class="name">St Charles' Church</h2>
-				<p lang="en" class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<p lang="et" class="about">ET Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<!-- Directions -->
-				<!-- Map -->
+			<div id="content-container">
+				<div class="pure-g">
+					<div class="pure-u-1 pure-u-md-1-2 about-me">
+						<img src="img/karli-kirik.jpg" class="photo" />
+						<h2 lang="en" class="name">Kaarli Kirik</h2>
+						<h2 lang="et" class="name">St Charles' Church</h2>
+						<p lang="en" class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<p lang="et" class="about">ET Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<!-- Directions -->
+						<!-- Map -->
+					</div>
+					<div class="pure-u-1 pure-u-md-1-2 about-me">
+						<img src="img/party-venue.jpg" class="photo" />
+						<h2 lang="en" class="name">Party venue</h2>
+						<h2 lang="et" class="name">ET Party venue</h2>
+						<p lang="en" class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<p lang="et" class="about">ET Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</div>
+				</div>
 			</div>
-			<div class="pure-u-1 pure-u-md-1-2 about-me">
-				<img src="img/party-venue.jpg" class="photo" />
-				<h2 lang="en" class="name">Party venue</h2>
-				<h2 lang="et" class="name">ET Party venue</h2>
-				<p lang="en" class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<p lang="et" class="about">ET Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			</div>
-		</div>
 
 <?php include 'inc/footer.php' ?>
+
+		</div>
 	</body>
 </html>
