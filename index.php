f14e98d7a8eb7e599a1f3afa28491b16e25b0840
solpyro <solpyro@gmail.com>
2022-03-05 12:36:46 +0200

    Formatting fixes

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 					<p class="text" lang="et">ET We hope to see you in Tallinn</p>
 				</div>
 
-				<div class="pure-g">
+				<div class="pure-g center">
 					<div class="pure-u-1 pure-u-md-1-2 about-me">
 						<img src="img/katrin.jpg" class="photo" />
 						<h2 class="name">Katrin</h2>
@@ -42,25 +42,25 @@
 						<p lang="en" class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 						<p lang="et" class="about">ET Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					</div>
-				</div>
 				
-				<div class="about-me">
-					<h2 lang="en" class="name">About us</h2>
-					<h2 lang="et" class="name">Meist</h2>
-					<p lang="en" class="about">
-						Stuart and Katrin first met at a Scout camp in Aegviidu, Estonia, in 2016, where they were put in charge of 30 British and Estonian scouts for 10 days. Over the next couple of years, they had many adventures together in Estonia, England and Finland.
-						<br /><br />
-						In 2018, Stuart finally took a leap across the continent and moved to Finland to be with Katrin, her daughters, Carmen and Carola, and their dog, Franky. One year later, they bought their own home, and two years after that their daughter, Cassandra, was born.
-						<br /><br />
-						Finally, exactly three years after moving to Finland, Stuart proposed to Katrin in the wee small hours of the morning September 11th 2021, and was over the moon that Katrin said “Jah Jah Jah!”
-					</p>
-					<p lang="et" class="about">
-						Stuart ja Katrin kohtusid esmakordselt 2016. Aasta suvel Eestis Aegviidus toimunud skaudilaagris, kus nad pandi 10 päevaks vastutama 30 Briti ja Eesti skaudi eest. Järgmise paari aasta jooksul kogesid nad  palju ühiseid seiklusi nii  Eestis, Inglismaal kui ka Soomes.
-						<br /><br />
-						2018. aastal tegi Stuart lõpuks hüppe üle kontinendi ja kolis Soome Katrini, tema tütarde Carmeni ja Carola ning nende koera Franky juurde. Aasta hiljem ostsid nad oma kodu ja kaks aastat pärast seda sündis nende vaike Cassandra.
-						<br /><br />
-						Ühtäkki, täpselt kolm aastat pärast Soome kolimist, tegi Stuart 11. septembril 2021 varajastel hommikutundidel Katrinile abieluettepaneku ja ei suutnud varjata oma rõõmu, kui Katrin vastas "Jah Jah Jah!"
-					</p>
+					<div class="pure-u-1 pure-u-md-2-3 about-me">
+						<h2 lang="en" class="name">About us</h2>
+						<h2 lang="et" class="name">Meist</h2>
+						<p lang="en" class="about">
+							Stuart and Katrin first met at a Scout camp in Aegviidu, Estonia, in 2016, where they were put in charge of 30 British and Estonian scouts for 10 days. Over the next couple of years, they had many adventures together in Estonia, England and Finland.
+							<br /><br />
+							In 2018, Stuart finally took a leap across the continent and moved to Finland to be with Katrin, her daughters, Carmen and Carola, and their dog, Franky. One year later, they bought their own home, and two years after that their daughter, Cassandra, was born.
+							<br /><br />
+							Finally, exactly three years after moving to Finland, Stuart proposed to Katrin in the wee small hours of the morning September 11th 2021, and was over the moon that Katrin said “Jah Jah Jah!”
+						</p>
+						<p lang="et" class="about">
+							Stuart ja Katrin kohtusid esmakordselt 2016. Aasta suvel Eestis Aegviidus toimunud skaudilaagris, kus nad pandi 10 päevaks vastutama 30 Briti ja Eesti skaudi eest. Järgmise paari aasta jooksul kogesid nad  palju ühiseid seiklusi nii  Eestis, Inglismaal kui ka Soomes.
+							<br /><br />
+							2018. aastal tegi Stuart lõpuks hüppe üle kontinendi ja kolis Soome Katrini, tema tütarde Carmeni ja Carola ning nende koera Franky juurde. Aasta hiljem ostsid nad oma kodu ja kaks aastat pärast seda sündis nende vaike Cassandra.
+							<br /><br />
+							Ühtäkki, täpselt kolm aastat pärast Soome kolimist, tegi Stuart 11. septembril 2021 varajastel hommikutundidel Katrinile abieluettepaneku ja ei suutnud varjata oma rõõmu, kui Katrin vastas "Jah Jah Jah!"
+						</p>
+					</div>
 				</div>
 			</div>
 
