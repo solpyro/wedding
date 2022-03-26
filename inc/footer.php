4f82fa0b3aded5bd29f157be483d12895c2172f7
solpyro <solpyro@gmail.com>
2022-03-26 21:41:00 +0200

    Add new portraits & add credit line to footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,5 @@
         <div class="footer">
             Katrin & Stuart<br />
             &copy; 2022
+            <div class="credit">Photos by <span>Rewan Kalki</span> and <a href="https://www.kumafoto.ee/">Paide Kuma Foto</a></div>
         </div>
